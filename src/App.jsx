0c8c4d8b640d317bac7606314695a5e3ff1a4c1a
Sean Swanson <sean-swan@hotmail.com>
2020-06-04 18:02:33 -0700

    Added Sass. Removed Boilerplate CRA code. Added stub components for some structural elements.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Header } from './partials/Header';
+import { Footer } from './partials/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      Simple
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
