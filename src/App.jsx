84f5db22dbdb0239dfed2c4a36faea692faeb15b
Sean Swanson <sean-swan@hotmail.com>
2020-06-09 16:36:28 -0700

    Adjusting styles and stubbing out more components.

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Header } from './partials/Header';
 import { Footer } from './partials/Footer';
 import './App.scss';
-const globe = require('./wikipedia-globe.png');
+
 const wikimedia = require('./wikimedia-logo.png');
+const globe = require('./wikipedia-globe.png');
 
 function App() {
   return (
@@ -11,13 +12,14 @@ function App() {
       <Header />
       <div className="content">
         <h1>
-          zen<span className="italic thin">wiki</span>
+          zen
+          <span className="italic thin">wiki</span>
         </h1>
         <form className="search-form" name="wikipedia-search-form">
-          <input className="search-input" type="text" />
-          <button>Go</button>
+          <input className="search-input soft" type="text" />
+          <button className="soft" type="submit">🔎</button>
         </form>
-        <div className="powered-by-wrapper">
+        <div className="powered-by-wrapper soft">
           <h2>Powered by</h2>
           <div className="logo-row">
             <img
---
+++ src/components/DisplayControls.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DisplayControls = () => (
+  <div>
+    DisplayControls
+  </div>
+);
+
+export default DisplayControls;
