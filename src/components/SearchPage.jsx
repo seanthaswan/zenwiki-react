39c12eba16a4e3ac6dfc6eb0c0767424b66700ce
Sean Swanson <sean-swan@hotmail.com>
2020-06-16 14:46:21 -0700

    Refactored colors into SCSS variables. Added logo.

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const logo = require('./128px-Japanese_Crest_mitu_Tomoe_Old_design-edit.png');
+const logo = require('./128px-Japanese_Crest_mitu_Tomoe_Old_design-edit-earth-wave.png');
 
 export const SearchPage = () => (
   <div className="SearchPage soft">
--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export const Header = () => (
   <header>
     <div className="header-controls-wrapper">
-      <div tabIndex="0" className="dot-focus soft button display-controls-button" />
-      <div tabIndex="0" className="dot-focus  soft button info-modal-button" />
+      <button type="button" aria-label="Display Settings" className="dot-focus soft"><div className="display-controls-button" /></button>
+      <button type="button" aria-label="App Info" className="dot-focus soft"><div className="info-modal-button" /></button>
     </div>
   </header>
 );
